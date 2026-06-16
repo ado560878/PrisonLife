8b7a09e88f0e7beb91407bb7908ec89a3b3f9154
ado560878 <ado560878@gmail.com>
2026-06-16 16:55:45 +0300

    Create ItemBuilder.java

---
+++ src/main/java/com/prisonlife/ItemBuilder.java
@@ -0,0 +1,25 @@
+package com.prisonlife;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class ItemBuilder {
+    private final ItemStack item;
+
+    public ItemBuilder(Material material) {
+        this.item = new ItemStack(material);
+    }
+
+    public ItemBuilder setName(String name) {
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        item.setItemMeta(meta);
+        return this;
+    }
+
+    public ItemStack build() {
+        return item;
+    }
+}
+s
