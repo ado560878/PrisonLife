b5f1ba0309992e1c056ebec626359e62363c968b
ado560878 <ado560878@gmail.com>
2026-06-16 17:06:52 +0300

    Update PrisonLifePlugin.java

--- src/main/java/com/prisonlife/PrisonLifePlugin.java
+++ src/main/java/com/prisonlife/PrisonLifePlugin.java
@@ -1,25 +1,11 @@
 package com.prisonlife;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import com.prisonlife.commands.JoinCommand;
-import com.prisonlife.listeners.PrisonListener;
-import com.prisonlife.listeners.GuardListener;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class PrisonLifePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("PrisonLife Modu Aktif!");
-
-        // Komutları kaydet
-        new JoinCommand(this);
-
-        // Listenerları kaydet
-        getServer().getPluginManager().registerEvents(new PrisonListener(), this);
-        getServer().getPluginManager().registerEvents(new GuardListener(), this);
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("PrisonLife Modu Kapatıldı.");
+        getCommand("join").setExecutor(new JoinCommand());
     }
 }
